9065a8b81549c80577d040c75ab21084828c865a
Wilson WeeSheng Khoo <wilson.khooweesheng@cognizant.com>
2024-10-01 16:55:06 +0800

    Use const instead of let

--- src/cli.ts
+++ src/cli.ts
@@ -307,7 +307,7 @@ const scanInit = async (argvs: Answers): Promise<string> => {
   }
 
   if (process.env.RUNNING_FROM_PH_GUI || process.env.OOBEE_VERBOSE) {
-    let randomTokenMessage = {
+    const randomTokenMessage = {
       type: 'randomToken',
       payload: `${data.randomToken}`,
     };
