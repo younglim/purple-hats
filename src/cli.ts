GovTechSG/purple-a11y

Resolve expected an assignment or function call and instead saw an expression

src/cli.ts
@@ -298,9 +298,11 @@ const scanInit = async (argvs: Answers): Promise<string> => {
 
   // File clean up after url check
   // files will clone a second time below if url check passes
-  process.env.PURPLE_A11Y_VERBOSE
-    ? deleteClonedProfiles(data.browser, data.randomToken)
-    : deleteClonedProfiles(data.browser); //first deletion
+  if (process.env.PURPLE_A11Y_VERBOSE) {
+    deleteClonedProfiles(data.browser, data.randomToken);
+  } else {
+    deleteClonedProfiles(data.browser); // first deletion
+  }
 
   if (argvs.exportDirectory) {
     constants.exportDirectory = argvs.exportDirectory;
@@ -329,9 +331,11 @@ const scanInit = async (argvs: Answers): Promise<string> => {
   await combineRun(data, screenToScan);
 
   // Delete cloned directory
-  process.env.PURPLE_A11Y_VERBOSE
-    ? deleteClonedProfiles(data.browser, data.randomToken)
-    : deleteClonedProfiles(data.browser); //second deletion
+  if (process.env.PURPLE_A11Y_VERBOSE) {
+    deleteClonedProfiles(data.browser, data.randomToken);
+  } else {
+    deleteClonedProfiles(data.browser); // second deletion
+  }
 
   // Delete dataset and request queues
   await cleanUp(data.randomToken);

younglim/purple-hats

src/cli.ts
@@ -298,9 +298,11 @@ const scanInit = async (argvs: Answers): Promise<string> => {
 
   // File clean up after url check
   // files will clone a second time below if url check passes
-  process.env.OOBEE_VERBOSE
-    ? deleteClonedProfiles(data.browser, data.randomToken)
-    : deleteClonedProfiles(data.browser); //first deletion
+  if (process.env.OOBEE_VERBOSE) {
+    deleteClonedProfiles(data.browser, data.randomToken);
+  } else {
+    deleteClonedProfiles(data.browser); // first deletion
+  }
 
   if (argvs.exportDirectory) {
     constants.exportDirectory = argvs.exportDirectory;
@@ -329,9 +331,11 @@ const scanInit = async (argvs: Answers): Promise<string> => {
   await combineRun(data, screenToScan);
 
   // Delete cloned directory
-  process.env.OOBEE_VERBOSE
-    ? deleteClonedProfiles(data.browser, data.randomToken)
-    : deleteClonedProfiles(data.browser); //second deletion
+  if (process.env.OOBEE_VERBOSE) {
+    deleteClonedProfiles(data.browser, data.randomToken);
+  } else {
+    deleteClonedProfiles(data.browser); // second deletion
+  }
 
   // Delete dataset and request queues
   await cleanUp(data.randomToken);