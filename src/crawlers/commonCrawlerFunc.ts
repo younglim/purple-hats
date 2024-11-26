2f0a01e967abc5ed4e508d066d73083900a75573
LeeYiXuan21 <S10243281@connect.np.edu.sg>
2024-11-26 14:32:48 +0800

    edit how to fix text for grading readability

--- src/crawlers/commonCrawlerFunc.ts
+++ src/crawlers/commonCrawlerFunc.ts
@@ -81,6 +81,8 @@ export const filterAxeResults = (
   const process = (item: ResultWithScreenshot, displayNeedsReview: boolean) => {
     const { id: rule, help: description, helpUrl, tags, nodes } = item;
 
+    //console.log(nodes)
+
     if (rule === 'frame-tested') return;
 
     const conformance = tags.filter(tag => tag.startsWith('wcag') || tag === 'best-practice');
@@ -362,6 +364,29 @@ export const runAxeScript = async ({
             {
               ...customAxeConfig.checks[2],
               evaluate: (_node: HTMLElement) => {
+                console.log('Readability flag check triggered');
+                if (gradingReadabilityFlag === '') {
+                  console.log('No readability issues detected');
+                  return true; // Pass if no readability issues
+                }
+                console.log('Readability issues detected');
+                // Dynamically update the grading messages
+                const gradingCheck = customAxeConfig.checks.find(
+                  check => check.id === 'oobee-grading-text-contents',
+                );
+                if (gradingCheck) {
+                  gradingCheck.metadata.messages.fail =
+                    'The text content is potentially difficult to read, with a Flesch-Kincaid Reading Ease score of ' +
+                    gradingReadabilityFlag +
+                    '.\nThe target passing score is above 50, indicating content readable by university students and lower grade levels.\nA higher score reflects better readability.';
+                }
+
+                return false; // Fail if readability issues are detected
+              },
+            },
+            {
+              ...customAxeConfig.checks[2],
+              evaluate: (_node: HTMLElement) => {
                 // console.log('Evaluate function triggered');
                 // if (flag === true) {
                 //   console.warn('Readability issues detected passed');
@@ -509,7 +534,11 @@ export const runAxeScript = async ({
     results.incomplete = await takeScreenshotForHTMLElements(results.incomplete, page, randomToken);
   }
 
+  console.log(results);
+
   const pageTitle = await page.evaluate(() => document.title);
+  //console.log('results', results);
+  //console.log('pageTitle', pageTitle);
   return filterAxeResults(results, pageTitle, customFlowDetails);
 };
 
